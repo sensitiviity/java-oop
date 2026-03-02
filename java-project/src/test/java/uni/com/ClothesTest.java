d6e7598c111c95dd3f433d444d84bd7595809d65
Valeriia <valeriaokh@gmail.com>
2026-03-02 14:35:53 +0100

    Add autotests

---
+++ java-project/src/test/java/uni/com/ClothesTest.java
@@ -0,0 +1,22 @@
+package uni.com;
+
+import uni.com.Clothes;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClothesTest {
+    @Test
+    void shouldThrowExceptionWhenInvalidValueInSetter() {
+        Clothes c = new Clothes("T-shirt", "white", "M",  22.50, "Nike", "cotton");
+        assertThrows(IllegalArgumentException.class, () -> {
+            c.setPrice(-10);
+        });
+    }
+
+    @Test
+    void shouldThrowExceptionWhenInvalidConstructorData(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Clothes("", "white", "M",  0, "Nike", "cotton");
+        });
+    }
+}
