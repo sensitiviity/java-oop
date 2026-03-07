4d260909ef8b3af0c3ae0da6a65deeab7e33e2c3
Valeriia <valeriaokh@gmail.com>
2026-03-07 13:53:12 +0100

    Add method for adding new clothes

--- java-project/src/main/java/uni/com/Store.java
+++ java-project/src/main/java/uni/com/Store.java
@@ -12,4 +12,28 @@ public class Store {
     public ArrayList<Clothes> getClothesList() {
         return clothesList;
     }
+
+    //adding
+
+    public void addNewClothes(Clothes cl, int quantity) {
+        if (cl == null || quantity <= 0) {
+            throw new IllegalArgumentException("Clothes is null or quantity <= 0");
+        }
+
+        int index = -1;
+        for (int i = 0; i < clothesList.size(); i++) {
+            if (clothesList.get(i).equals(cl)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index >= 0) {
+            int oldQ = quantities.get(index);
+            quantities.set(index, oldQ + quantity);
+        } else {
+            clothesList.add(cl);
+            quantities.add(quantity);
+        }
+    }
 }
